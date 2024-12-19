48c055a6fa4c7ed8db4702f9ee82a8e56f53f289
Joshua. C <76528955+jemcap@users.noreply.github.com>
2024-12-19 13:46:18 +0000

    Fix: Implemented functionality to route to the Wallet page when a wallet is not connected when trying to buy an NFT

--- src/components/Collection/nftcardcollection.component.jsx
+++ src/components/Collection/nftcardcollection.component.jsx
@@ -1,9 +1,11 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { CartContext } from "../../context/CartContext";
+import { WalletContext } from "../../context/walletContext";
 
 const NftCard = ({ id, image, title, price }) => {
   const { addToCart } = useContext(CartContext);
+  const { isWalletConnected } = useContext(WalletContext);
 
   const handleAddToCart = () => {
     const item = { id, image, title, price };
@@ -18,22 +20,41 @@ const NftCard = ({ id, image, title, price }) => {
           alt=""
         />
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-          <button
-            onClick={handleAddToCart}
-            className="flex items-center p-6 w-[248px] h-[67px] text-[16px] justify-center bg-[#FAFAFA] text-black rounded-full"
-          >
-            <span>Buy</span>
-            <svg
-              width="24"
-              height="20"
-              xmlns="http://www.w3.org/2000/svg"
-              fillRule="evenodd"
-              clipRule="evenodd"
-              className="ml-2  flex justify-center"
+          {!isWalletConnected ? (
+            <Link
+              to="/wallet"
+              className="flex items-center p-6 w-[248px] h-[67px] text-[16px] justify-center bg-[#FAFAFA] text-black rounded-full"
             >
-              <path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z" />
-            </svg>
-          </button>
+              <span>Buy</span>
+              <svg
+                width="24"
+                height="20"
+                xmlns="http://www.w3.org/2000/svg"
+                fillRule="evenodd"
+                clipRule="evenodd"
+                className="ml-2  flex justify-center"
+              >
+                <path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z" />
+              </svg>
+            </Link>
+          ) : (
+            <button
+              onClick={handleAddToCart}
+              className="flex items-center p-6 w-[248px] h-[67px] text-[16px] justify-center bg-[#FAFAFA] text-black rounded-full"
+            >
+              <span>Buy</span>
+              <svg
+                width="24"
+                height="20"
+                xmlns="http://www.w3.org/2000/svg"
+                fillRule="evenodd"
+                clipRule="evenodd"
+                className="ml-2  flex justify-center"
+              >
+                <path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z" />
+              </svg>
+            </button>
+          )}
         </div>
       </div>
       <div>
